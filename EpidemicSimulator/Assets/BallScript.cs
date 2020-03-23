30f3200898a2d14c1b75dd97fc00e83a8a85ed82
Ramon West <jgomezcu@asu.edu>
2020-03-23 16:23:13 -0700

    EpidemicSimulator

---
+++ EpidemicSimulator/Assets/BallScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+
+    public bool healthy;
+    public bool infected;
+    public bool frozen;
+
+    Vector3[] originalVertices;
+    Vector3[] spikyVertices;
+
+    MeshRenderer meshRenderer;
+    MeshFilter meshFilter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshFilter = GetComponent<MeshFilter>();
+        originalVertices = meshFilter.mesh.vertices;
+
+        spikyVertices = new Vector3[originalVertices.Length];
+
+        for (int i = 0; i < spikyVertices.Length; i++) {
+            spikyVertices[i] = originalVertices[i] * Random.value;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthy) {
+            meshRenderer.material.color = new Color(0.3f, 0.3f, 1);
+            meshFilter.mesh.vertices = originalVertices;
+
+        }
+
+        else if (infected){
+            meshRenderer.material.color = new Color(1, 0.3f, 0.3f);
+            meshFilter.mesh.vertices = spikyVertices;
+
+        }
+
+        else {
+            meshRenderer.material.color = new Color(0.3f, 1, 0.3f);
+            meshFilter.mesh.vertices = originalVertices;
+
+        }
+    }
+}
---
+++ EpidemicSimulator/Assets/Population.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Population : MonoBehaviour
+{
+    BallScript[] persons;
+
+    public int numPersons;
+    public int numInfected;
+    public int numFrozen;
+    public GameObject personPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        persons = new BallScript[numPersons];
+
+        for (int i = 0; i < numPersons; i++){
+            Vector3 position = new Vector3(1, 0, 1);
+            while(Physics.OverlapSphere(position, personPrefab.transform.localScale.x/2).Length > 0){
+                position = new Vector3(Random.value * 20, 0.5f, Random.value * 20);
+            }
+            GameObject personClone = Instantiate(personPrefab, position, Quaternion.identity);
+            persons[i] = personClone.GetComponent<BallScript>();
+
+            if (i < numInfected){
+                persons[i].healthy = false;
+                persons[i].infected = true;
+            }
+            else {
+                persons[i].healthy = true;
+                persons[i].infected = false;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
